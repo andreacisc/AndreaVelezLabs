cedae5719a97002531dc7c055be803ef7e8595b0
andreacisc <andreavelez6555@gmail.com>
2025-10-03 23:43:31 -0400

    added lab 2 files

---
+++ lab2/icon.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+@export var speed = 10
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("move_down"):
+		position.y += speed
+	if Input.is_action_pressed("move_up"):
+		position.y -= speed
+	if Input.is_action_pressed("move_right"):
+		position.x += speed
+	if Input.is_action_pressed("move_left"):
+		position.x -= speed
+		
+	if Input.is_action_just_pressed("jump"):
+		print("I jumped!")
